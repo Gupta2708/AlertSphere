50b60fb482e28dd94cb90c92c8d2d4331feb36f4
Gupta2708 <guptavaibhav2708@gmail.com>
2025-11-03 23:00:07 +0530

    edit db

--- backend/app/utils/db.py
+++ backend/app/utils/db.py
@@ -1,8 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import os
+from sqlalchemy.engine.url import make_url
 
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./alertsphere.db")
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+
+connect_args = {}
+if make_url(DATABASE_URL).get_backend_name() == "sqlite":
+    connect_args = {"check_same_thread": False}
+
+engine = create_engine(DATABASE_URL, connect_args=connect_args)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
